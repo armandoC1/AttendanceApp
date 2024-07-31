bdf557fc8b1e3dec1215003e9de4dd2d7b69abb8
armandoC1 <hr24006@esfe.agape.edu.sv>
2024-07-30 22:12:38 -0600

    grupos

---
+++ src/main/java/org/esfe/modelos/Grupo.java
@@ -0,0 +1,42 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "grupos")
+public class Grupo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "El nombre es requerido") String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(@NotBlank(message = "El nombre es requerido") String nombre) {
+        this.nombre = nombre;
+    }
+
+    public @NotBlank(message = "La descripcion es requerida") String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(@NotBlank(message = "La descripcion es requerida") String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @NotBlank(message = "La descripcion es requerida")
+    private String descripcion;
+}
---
+++ src/main/java/org/esfe/servicios/interfaces/IGrupoService.java
@@ -0,0 +1,22 @@
+package org.esfe.servicios.interfaces;
+
+import org.esfe.modelos.Grupo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IGrupoService {
+    Page<Grupo> buscarTodosPaginados(Pageable pageable);
+
+    List<Grupo> obtenerTodos();
+
+    Optional<Grupo> buscarPorId(Integer id);
+
+    Grupo crearOEditra(Grupo grupo);
+
+    void eliminarPorId(Integer id);
+
+
+}
